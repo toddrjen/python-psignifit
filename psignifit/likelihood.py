fix incorrect likelihood and logLikelihood argument handling and fix places where incorrect handling was used

psignifit/likelihood.py
@@ -2,7 +2,7 @@
 import numpy as np
 import scipy.special as sp
 
-def likelihood(data, options, args):
+def likelihood(data, options, *args):
     """
     Calculates the (normalized) likelihood for the data from given parameters
     function [p,logPmax] = likelihood(typeHandle,data,alpha,beta,lambda,gamma)
@@ -13,7 +13,7 @@ def likelihood(data, options, args):
 
     """
     
-    p = logLikelihood(data, options, args)
+    p = logLikelihood(data, options, *args)
         
     '''We never need the actual value of the likelihood. Something proportional
     is enough and this circumvents numerical problems for the likelihood to
@@ -27,7 +27,7 @@ def likelihood(data, options, args):
     return (p,logPmax)
 
 
-def logLikelihood(data,options, args):
+def logLikelihood(data,options, alpha, beta, lambda=0, gamma=0.5, varscale=1):
     """
     The core function to evaluate the logLikelihood of the data
     function p=logLikelihood(data,options,alpha,beta,lambda,gamma,varscale)
@@ -39,28 +39,13 @@ def logLikelihood(data,options, args):
     
     """
 
-    
     sigmoidHandle = options['sigmoidHandle']
-    if len(args) < 2:
-        raise ValueError('not enough input parameters')
-    else:
-        alpha = np.atleast_1d(args[0])
-        beta = np.atleast_1d(args[1])
-            
-        if len(args) > 2:
-            lamb = atleast_1d(args[2])
-        else:
-            lamb = np.array([0])
-            
-        if len(args) > 3:
-            gamma = atleast_1d(args[3])
-        else:
-            gamma = np.array([0.5])
-            
-        if len(args) > 4:
-            varscale = atleast_1d(args[4])
-        else:
-            varscale = np.array([1])
+    
+    alpha = np.atleast_1d(args[0])
+    beta = np.atleast_1d(args[1])
+    lamb = atleast_1d(args[2])
+    gamma = atleast_1d(args[3])
+    varscale = atleast_1d(args[4])
     
     # is the input only one point?
     oneParameter = not(len(alpha) > 1 or len(beta) > 1 or len(lamb) > 1 

psignifit/psignifit.py
@@ -370,7 +370,7 @@ def psignifitCore(data, options):
                     
     '''Evaluate likelihood and form it into a posterior'''
     
-    (result['Posterior'], result['logPmax']) = _l.likelihood(data, options, result['X1D'])
+    (result['Posterior'], result['logPmax']) = _l.likelihood(data, options, *result['X1D'])
     result['weight'] = getWeights(result['X1D'])
     integral = np.sum(np.array(result['Posterior'][:])*np.array(result['weight'][:]))
     result['Posterior'] = result['Posterior']/integral
@@ -400,21 +400,21 @@ def psignifitCore(data, options):
             Fit[idx] = result['X1D'][idx][index[idx]] 
         
         if options['expType'] == 'YesNo':
-            fun = lambda X, f: -_l.logLikelihood(data, options, [X[0],X[1],X[2],X[3],X[4]])
+            fun = lambda X, f: -_l.logLikelihood(data, options, X[0],X[1],X[2],X[3],X[4])
             x0 = _deepcopy(Fit)
             a = None
             
         elif options['expType'] == 'nAFC':
             #def func(X,f):
-            #    return -_l.logLikelihood(data,options, [X[0], X[1], X[2], f, X[3]])
+            #    return -_l.logLikelihood(data,options, X[0], X[1], X[2], f, X[3])
             #fun = func
-            fun = lambda X, f:  -_l.logLikelihood(data,options, [X[0], X[1], X[2], f, X[3]])
+            fun = lambda X, f:  -_l.logLikelihood(data,options, X[0], X[1], X[2], f, X[3])
             x0 = _deepcopy(Fit[0:3]) # Fit[3]  is excluded
             x0 = np.append(x0,_deepcopy(Fit[4]))
             a = np.array([1/options['expN']])
             
         elif options['expType'] == 'equalAsymptote':
-            fun = lambda X, f: -_l.logLikelihood(data,options,[X[0], X[1], X[2], f, X[3]])
+            fun = lambda X, f: -_l.logLikelihood(data,options,X[0], X[1], X[2], f, X[3])
             x0 = _deepcopy(Fit[0:3])
             x0 = np.append(x0,_deepcopy(Fit[4]))
             a =  np.array([np.nan])